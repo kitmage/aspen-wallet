22fb610c71760bef397fca916a23f3e35aa77444
KitMage.com <admin@kitmage.com>
2026-05-15 15:40:51 -0500

    Use FluentCRM profile sections filter for Wallet tab

--- includes/fluentcrm.php
+++ includes/fluentcrm.php
@@ -10,6 +10,7 @@ function aspen_wallet_register_fluentcrm_hooks() {
 
 	add_action( 'fluent_crm/after_init', 'aspen_wallet_fluentcrm_register_profile_section', 20 );
 	add_action( 'fluentcrm_loaded', 'aspen_wallet_fluentcrm_register_profile_section', 20 );
+	add_filter( 'fluentcrm_profile_sections', 'aspen_wallet_fluentcrm_add_profile_tab', 20, 1 );
 }
 
 function aspen_wallet_fluentcrm_has_extender_profile_api() {
@@ -37,6 +38,20 @@ function aspen_wallet_fluentcrm_register_profile_section() {
 		__( 'Wallet', 'aspen-wallet' ),
 		'aspen_wallet_fluentcrm_profile_section_callback'
 	);
+}
+
+function aspen_wallet_fluentcrm_add_profile_tab( $sections ) {
+	if ( ! is_array( $sections ) ) {
+		$sections = array();
+	}
+
+	$sections['wallet'] = array(
+		'slug'  => 'wallet',
+		'title' => __( 'Wallet', 'aspen-wallet' ),
+		'icon'  => 'el-icon-wallet',
+	);
+
+	return $sections;
 }
 
 
